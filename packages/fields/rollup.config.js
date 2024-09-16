0f1387c43f8d6a745f6a35bb6d0f0ef0175bd20e
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2024-09-16 11:23:43 +0530

    chore: config changes

--- packages/fields/rollup.config.js
+++ packages/fields/rollup.config.js
@@ -1,21 +1,31 @@
 const { withNx } = require("@nx/rollup/with-nx");
 const preserveDirectives = require("rollup-plugin-preserve-directives");
 const terser = require("@rollup/plugin-terser");
+const fg = require("fast-glob");
 
-module.exports = withNx(
-  {
-    main: "./src/index.ts",
-    outputPath: "../../dist/packages/fields",
-    tsConfig: "./tsconfig.lib.json",
-    compiler: "swc",
-    external: ["react", "react-dom", "react/jsx-runtime"],
-    format: ["cjs", "esm"],
-    assets: [{ input: ".", output: ".", glob: "README.md" }],
-  },
-  {
-    plugins: [
-      preserveDirectives.default(), // For preserving "use client" directives
-      terser(), // For minification
-    ],
-  },
-);
+module.exports = async () => {
+  const inputs = await fg("./src/**/*.{ts,tsx}", {
+    onlyFiles: true,
+    cwd: __dirname,
+  });
+
+  return withNx(
+    {
+      main: "./src/index.ts",
+      outputPath: "../../dist/packages/fields",
+      tsConfig: "./tsconfig.lib.json",
+      compiler: "swc",
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      format: ["cjs", "esm"],
+      assets: [{ input: ".", output: ".", glob: "README.md" }],
+    },
+    {
+      input: inputs,
+      output: { preserveModules: true },
+      plugins: [
+        preserveDirectives.default(), // For preserving "use client" directives
+        terser(), // For minification
+      ],
+    },
+  );
+};
