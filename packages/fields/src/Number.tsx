211cbe2ba322c588669c3c0f060bb15d178be2b9
Aditya Mathur <mathuraditya724@gmail.com>
2025-01-10 10:40:56 +0530

    fix(fd): added step in props

--- packages/fields/src/Number.tsx
+++ packages/fields/src/Number.tsx
@@ -10,18 +10,24 @@ export type NumberProps = {
   min?: InputField["min"];
   max?: InputField["max"];
   defaultValue?: number;
+  step?: number;
   value?: number;
   onChange?: (value?: number) => void;
 };
 
-export function NumberField({ type, onChange, ...props }: NumberProps) {
+export function NumberField({
+  type,
+  onChange,
+  step = 1,
+  ...props
+}: NumberProps) {
   return (
     <InputWrapper>
       <RaftyInputField
         {...props}
         id={props.name}
         type="number"
-        step="1"
+        step={step}
         onChange={(event) => {
           const value = event.target.value;
 
