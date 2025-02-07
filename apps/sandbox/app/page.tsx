995e9f1f343abbf85acac7a11b7ffda44b00c3d9
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2025-02-07 16:35:26 +0530

    feat: integrate axios to fetch student data from external API

--- apps/sandbox/app/page.tsx
+++ apps/sandbox/app/page.tsx
@@ -2,8 +2,27 @@
 import { Blueprint, DuckField, DuckForm } from "duck-form";
 import { components } from "./config";
 import { schema } from "./schema";
+import axios from "axios";
 
 export default function HomePage() {
+  const student = axios.get(
+    "https://api.rhinobase.io/api/organisations/dez_erp/collections/student/6777952984427e9bd2beca47",
+    {
+      headers: {
+        Authorization:
+          "Bearer xG0oWsJJlm0YpdDvGjKxtLDy8h9kRjKjjwQGo5FZerrWh1YHN3X57nUvjiyYstUm0Cc3bvbBtezHp3DYInYanjUbOpr5zJfCrN_gZXLHr8fQ",
+      },
+    }
+  );
+
+  const data = {
+    c: {
+      queries: {
+        student,
+      },
+    },
+  };
+
   return (
     <DuckForm
       components={components}
