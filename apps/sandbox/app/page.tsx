3980a937eb18bfb34c77193062eca45ad97110f0
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-02-07 16:46:44 +0530

    chore: minor changes

--- apps/sandbox/app/page.tsx
+++ apps/sandbox/app/page.tsx
@@ -3,6 +3,7 @@ import { Blueprint, DuckField, DuckForm } from "duck-form";
 import { components } from "./config";
 import { schema } from "./schema";
 import axios from "axios";
+import nunjucks from "nunjucks";
 
 export default function HomePage() {
   const student = axios.get(
@@ -10,12 +11,12 @@ export default function HomePage() {
     {
       headers: {
         Authorization:
-          "Bearer xG0oWsJJlm0YpdDvGjKxtLDy8h9kRjKjjwQGo5FZerrWh1YHN3X57nUvjiyYstUm0Cc3bvbBtezHp3DYInYanjUbOpr5zJfCrN_gZXLHr8fQ",
+          "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImJaLV81ZyJ9.eyJpc3MiOiJodHRwczovL3Nlc3Npb24uZmlyZWJhc2UuZ29vZ2xlLmNvbS9hbGNhbnRhcmEtaW8iLCJuYW1lIjoiQWRpdHlhIE1hdGh1ciIsImF1ZCI6ImFsY2FudGFyYS1pbyIsImF1dGhfdGltZSI6MTczODkwMTA1OSwidXNlcl9pZCI6Ing0cnJmalFyak9YQmpybnJxWHJKeDFtZmR2dDEiLCJzdWIiOiJ4NHJyZmpRcmpPWEJqcm5ycVhySngxbWZkdnQxIiwiaWF0IjoxNzM4OTAxMDYwLCJleHAiOjE3Mzk1MDU4NjAsImVtYWlsIjoibWF0aHVyYWRpdHlhNzI0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJtYXRodXJhZGl0eWE3MjRAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.y4LKfIo_oF22eliRPPS2P76mAjIfo5QQHO6a5O_fYaNHwqQuPcphJ1Ja38SXbyA_Hh0KljIQbFeOX4XGNWlVlEHKMMdWcdqF_7Yg5r3kcmiCekiCk_NlbgAiI-xzAXR28lUQMAzCaJjXwDk0PZavmyQox7-BeyoDoX9nDM0X6jvsitibTXIq-x23VNMG92fJZdDJUTlAXtkIa2GqOYz3lC7ckiA12tN5yaT_7p6WJnJ4Mk0kMvahVDjO3rOObTXO1l4bvbETzObdmwPVxkZJKJFNgJHtm-Q12lzTvwUm-POtW5rizCDVRIwi4wLzi2iWWW2sqkJ6bjEimbOuT3H2vg",
       },
-    }
+    },
   );
 
-  const data = {
+  const context = {
     c: {
       queries: {
         student,
@@ -23,13 +24,17 @@ export default function HomePage() {
     },
   };
 
+  const renderedValue = JSON.parse(
+    nunjucks.renderString(JSON.stringify(schema), context),
+  );
+
   return (
     <DuckForm
       components={components}
       generateId={(_, props) => (props.id ? String(props.id) : undefined)}
     >
-      <Blueprint schema={schema}>
-        {Object.keys(schema).map((key) => (
+      <Blueprint schema={renderedValue}>
+        {Object.keys(renderedValue).map((key) => (
           <DuckField key={key} id={key} />
         ))}
       </Blueprint>
